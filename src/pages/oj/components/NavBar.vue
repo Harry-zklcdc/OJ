51b279a92376d1af61b42164722364fec32fd183
xiao-gy <45650719+xiao-gy@users.noreply.github.com>
2019-03-24 19:19:11 +0800

    修改换肤按钮样式不同问题
    修改换肤按钮样式不同问题（手残改的）

--- src/pages/oj/components/NavBar.vue
+++ src/pages/oj/components/NavBar.vue
@@ -46,7 +46,7 @@
         </Menu-item>
       </Submenu>
 	  
-          <Dropdown trigger="click" @on-click="switchChange" class="ivu-menu-submenu">
+          <Dropdown trigger="click" @on-click="switchChange" class="ivu-dropdown-menu">
             <div>换肤
               <Icon type="arrow-down-b"></Icon>
             </div>
