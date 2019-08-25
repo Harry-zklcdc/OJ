6add6e7dbb1c60dd1a85b8c5bc62521ac1121010
Harry-zklcdc <1269158832@qq.com>
2019-08-25 21:59:44 +0800

    修改看板娘大小

--- static/plugin/live2d/waifu-tips.js
+++ static/plugin/live2d/waifu-tips.js
@@ -52,8 +52,8 @@ live2d_settings['showCopyMessage']      = false;         // 显示 复制内容 
 live2d_settings['showWelcomeMessage']   = true;         // 显示进入面页欢迎词
 
 //看板娘样式设置
-live2d_settings['waifuSize']            = '280x250';    // 看板娘大小，例如 '280x250', '600x535'
-live2d_settings['waifuTipsSize']        = '250x70';     // 提示框大小，例如 '250x70', '570x150'
+live2d_settings['waifuSize']            = '210x200';    // 看板娘大小，例如 '280x250', '600x535'
+live2d_settings['waifuTipsSize']        = '200x70';     // 提示框大小，例如 '250x70', '570x150'
 live2d_settings['waifuFontSize']        = '12px';       // 提示框字体，例如 '12px', '30px'
 live2d_settings['waifuToolFont']        = '14px';       // 工具栏字体，例如 '14px', '36px'
 live2d_settings['waifuToolLine']        = '20px';       // 工具栏行高，例如 '20px', '36px'
