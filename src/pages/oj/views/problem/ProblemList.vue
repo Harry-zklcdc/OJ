a409f5c06a341356a53dfc4538142c3a280a7048
unknown <1269158832@qq.com>
2020-04-19 23:05:09 +0800

    [Fix] DisPlay width

--- src/pages/oj/views/problem/ProblemList.vue
+++ src/pages/oj/views/problem/ProblemList.vue
@@ -92,7 +92,7 @@
           {
             title: '#',
             key: '_id',
-            width: 80,
+            width: 98,
             render: (h, params) => {
               return h('Button', {
                 props: {
