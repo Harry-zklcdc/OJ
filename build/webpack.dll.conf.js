da006644cdfdadedb41aeefc6e91efd1160756d1
Harry-zklcdc <1269158832@qq.com>
2021-06-28 13:19:49 +0800

    [Upgrade] Support Node.JS 12.16.1

--- build/webpack.dll.conf.js
+++ build/webpack.dll.conf.js
@@ -27,7 +27,12 @@ const globOptions = {cwd: resolve('static/js'), absolute: true};
 let oldDlls = glob.sync('vendor.dll.*.js', globOptions);
 console.log("cleaning old dll..")
 oldDlls.forEach(f => {
-  fs.unlink(f)
+  fs.unlink(f, function (err) {
+    if (err){
+        print("Delete Fail");
+        return;
+    }
+  })
 })
 console.log("building ..")
 
